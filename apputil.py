3fbfa373453e83ddf5b7c14ab6ecf5dccbfdbacd
varunrapolu1612 <rvarun@iu.edu>
2025-09-07 22:12:22 -0400

    Update apputil.py

--- apputil.py
+++ apputil.py
@@ -1,13 +1,27 @@
 import numpy as np
 
-
-# update/add code below ...
-
 def ways(n):
-    return None
+    '''
+    def ways(n):
+    x=int(n//5)
+    ways = list()
+    for i in range(x+1):  
+        ways.append((n-(5*i), i))
+    return ways
+    '''
+    if n<0:
+        return 0
+    else:
+        return (int(n//2) + 1)
 
 def lowest_score(names, scores):
-    return None
+    # indices that sort the scores in ascending order
+    sorted_indices = np.argsort(scores)
+    return names[sorted_indices]
 
 def sort_names(names, scores):
-    return None
+    # indices that sort the scores in ascending order
+    sorted_indices = np.argsort(scores)
+    # Reverse for descending order
+    sorted_indices = sorted_indices[::-1]
+    return names[sorted_indices]
